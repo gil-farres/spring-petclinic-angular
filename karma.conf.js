70c4784f156f0d2698d426c740821b89a826380b
gil-farres <gil.farres@uvic.cat>
2025-11-15 18:11:59 +0100

    prova jenkins 4

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,3 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
 module.exports = function (config) {
   config.set({
     browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
@@ -17,22 +14,28 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-junit-reporter') // <-- afegir aquí
     ],
     client:{
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
+      dir: require('path').join(__dirname, 'coverage'),
+      reports: [ 'html', 'lcovonly' ],
       fixWebpackSourcePaths: true
     },
-
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'junit'], // <-- afegir 'junit'
+    junitReporter: {
+      outputDir: 'test-results',  // <-- carpeta on es generaran els XML
+      outputFile: 'unit.xml',
+      useBrowserName: false
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: true // <-- posar true per Jenkins
   });
 };
